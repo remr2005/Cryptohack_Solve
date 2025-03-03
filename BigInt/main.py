e3c142dabfe7565956417d34a07e6be87c2fb3bb
Kemran <morpeh2005ka@gmail.com>
2025-03-03 19:18:51 +0300

    Насранно

---
+++ BigInt/main.py
@@ -0,0 +1,8 @@
+from Crypto.Util.number import *
+
+def main():
+    b = long_to_bytes(11515195063862318899931685488813747395775516287289682636499965282714637259206269)
+    print(b)
+
+if __name__ == "__main__":
+    main()
